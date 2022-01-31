85eb2262c806a460ea486133a91a1ae07201a9f8
Kris Cao <70863070+luckygy618@users.noreply.github.com>
2022-01-31 04:56:02 -0500

    Delete ProSkate_website_backend-main directory

--- ProSkate_website_backend-main/backend/jest.config.e2e.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  verbose: true,
-  roots: ['./test/e2e'],
-  testTimeout: 5000,
-};
--- ProSkate_website_backend-main/backend/server.js
+++
@@ -1,8 +0,0 @@
-var debug = require('debug')('api:server');
-var app = require('./app');
-
-const HTTP_PORT = process.env.PORT || 8080;
-
-app.listen(HTTP_PORT, () => {
-  debug('Ready to handle requests on port ' + HTTP_PORT);
-});
